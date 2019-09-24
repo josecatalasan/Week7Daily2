676b98ea0c3c1a6b1ca397702fb0526a2c235854
Jose Catalasan <jose.catalasan@mobileappscompany.us>
2019-09-24 17:10:04 -0400

    hide soft keyboard on button press, added the ability to remove markers

--- Week7Daily2Navigation/app/src/main/java/com/example/week7daily2navigation/view/activities/MapsActivity.kt
+++ Week7Daily2Navigation/app/src/main/java/com/example/week7daily2navigation/view/activities/MapsActivity.kt
@@ -11,33 +11,43 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
 
     private lateinit var map: GoogleMap
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        map.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener{
+            override fun onMarkerDragStart(marker: Marker?) { marker?.remove() }
+                override fun onMarkerDragEnd(p0: Marker?) {}
+                override fun onMarkerDrag(p0: Marker?) {}
+        })
+
+        //Center camera on USA
         map.moveCamera(CameraUpdateFactory.newLatLng(LatLng(38.0,-97.0)))
         map.moveCamera(CameraUpdateFactory.zoomTo(3.0f))
+
         //set Map properties
         //map.mapType = GoogleMap.MAP_TYPE_HYBRID
     }
 
     private fun displayLocationOnMap(latLng : LatLng, title : String){
-        map.addMarker(MarkerOptions().position(latLng).title(title))
+        var marker = map.addMarker(MarkerOptions().position(latLng).title(title).draggable(true))
         map.moveCamera(CameraUpdateFactory.newLatLng(latLng))
-        map.moveCamera(CameraUpdateFactory.zoomTo(10.0f))
+        map.animateCamera(CameraUpdateFactory.zoomTo(13.0f))
     }
 
     //Geocoding
@@ -62,6 +72,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         if(address.isNotEmpty()){
             retrievedLatLng = getLocationByAddress(address)
             displayLocationOnMap(retrievedLatLng, getLocationByLatLng(retrievedLatLng))
+        }
+        hideKeyboard()
+    }
+
+    private fun hideKeyboard() {
+        val view = this.currentFocus
+        view?.let { v ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.let { it.hideSoftInputFromWindow(v.windowToken, 0) }
         }
     }
 }
